eb6ce9904176228f3d70aa0fed9109df233f952a
Yudchits <pashka.yudchits@gmail.com>
2024-09-25 18:39:41 +0300

    added fade service for opacity animating

---
+++ src/common/services/fade.service.js
@@ -0,0 +1,36 @@
+let instance;
+
+class FadeService {
+    constructor() {
+        if (!instance) {
+            instance = this;
+        }
+
+        return instance;
+    }
+
+    animate() {
+        document.addEventListener('scroll', this.onScroll);
+        this.onScroll();
+    }
+
+    async onScroll() {
+        const fadeItems = document.querySelectorAll('._fade-animation');
+        const delay = 300;
+    
+        for (const fadeItem of fadeItems) {
+            const rect = fadeItem.getBoundingClientRect();
+            const distanceFromTop = rect.top + window.scrollY;
+            const scrollY = window.scrollY;
+            const windowHeight = window.innerHeight;
+    
+            if (distanceFromTop >= scrollY && distanceFromTop < (windowHeight + scrollY)) {
+                fadeItem.classList.add('_faded');
+                await new Promise(resolve => setTimeout(resolve, delay));
+            }
+        }
+    }    
+    
+}
+
+export default FadeService;
